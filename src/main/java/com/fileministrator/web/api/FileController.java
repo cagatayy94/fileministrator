8be1c9e8634bc05fe2d5d0e8d3d26bd659a63455
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2022-04-15 16:28:37 +0300

    Implemented CREATE READ methods on Controller

--- src/main/java/com/fileministrator/web/api/FileController.java
+++ src/main/java/com/fileministrator/web/api/FileController.java
@@ -3,31 +3,48 @@ package com.fileministrator.web.api;
 import com.fileministrator.web.business.FileManager;
 import com.fileministrator.web.entity.File;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/file")
+@RequestMapping("api/v1/files")
 public class FileController {
-
-    @Autowired
     FileManager fileManager;
 
-    @GetMapping
-    ResponseEntity<List<File>>getFiles(){
-        return ResponseEntity.ok(
-                this.fileManager.getAll()
-        ) ;
+    @Autowired
+    public FileController(FileManager fileManager) {
+        this.fileManager = fileManager;
     }
 
-    @GetMapping
-    ResponseEntity<File>getOne(Integer id){
+    @GetMapping()
+    ResponseEntity<List<File>>getAll(){
         return ResponseEntity.ok(
-                this.fileManager.get(id)
-        ) ;
+                this.fileManager.getAll()
+        );
+    }
+
+    @GetMapping("{fileId}")
+    ResponseEntity<File>getById(@PathVariable Integer fileId){
+        return ResponseEntity.ok(
+                this.fileManager.getFirstById(fileId)
+        );
+    }
+
+    @RequestMapping(
+            method = RequestMethod.POST,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    ResponseEntity<String> create(@RequestPart("file") MultipartFile file) {
+        try {
+            this.fileManager.create(file);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Success");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 }
