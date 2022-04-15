3fe3fe7a3f3416f9fa9092f3319d37ee3cde3354
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2022-04-15 16:27:52 +0300

    Updated repository

--- src/main/java/com/fileministrator/web/repository/FileRepository.java
+++ src/main/java/com/fileministrator/web/repository/FileRepository.java
@@ -4,4 +4,5 @@ import com.fileministrator.web.entity.File;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FileRepository extends JpaRepository<File, Integer> {
+    File getFirstById(Integer fileId);
 }
