3dc5a58a1b95280f55af0d146d20f2e58acfed76
Mustafa Çağatay Yılmaz <mustafa.cagatay.yilmaz@gmail.com>
2022-04-17 21:08:26 +0300

    Updated service for work real database users

--- src/main/java/com/fileministrator/web/auth/JwtUserDetailsService.java
+++ src/main/java/com/fileministrator/web/auth/JwtUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.fileministrator.web.auth;
 
+import com.fileministrator.web.business.UserManager;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,12 +12,19 @@ import java.util.ArrayList;
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
 
+    private final UserManager userManager;
+
+    public JwtUserDetailsService(UserManager userManager) {
+        this.userManager = userManager;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("cagatay".equals(username)) {
-            return new User("cagatay", "$2a$12$/r3M3it2ND7sdniQ3lc7hOUdH/Atp11rt0YTlXqb3jsbPOroHoSzu",
-                    new ArrayList<>());
-        } else {
+        com.fileministrator.web.entity.User user = this.userManager.getFirstByEmail(username);
+
+        if (user != null){
+            return new User(user.getEmail(), user.getPassword(), new ArrayList<>());
+        }else{
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
     }
